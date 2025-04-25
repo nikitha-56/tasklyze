a9cf59767ca576d267c4e29a9b9d17d18d8167a8
nikitha-56 <nikithatalluri1@gmail.com>
2025-04-25 18:53:47 +0530

    installed node modules

---
+++ reactjs/src/App.js
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello I am nikki</h1>
+        <p> welcome </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
